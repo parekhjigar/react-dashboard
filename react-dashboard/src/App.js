b5019d7c246b7d06c375582ba4575af6cc0d43a9
parekhjigar <hello@parekhjigar.com>
2020-06-17 22:20:57 +1000

    Added containers for all comonents

--- react-dashboard/src/App.js
+++ react-dashboard/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Container, Nav } from "./styled-components";
-//import logo from './logo.svg';
-import './App.css';
+import UserImg from "./assets/images/user-img-placeholder.jpeg";
 import config from './config';
 import "bootstrap/dist/css/bootstrap.css";
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${ config.spreadsheetId }/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${ config.apiKey }`;
@@ -36,20 +35,162 @@ class App extends Component {
 
   render() {
     return (
-      <Nav className="navbar navbar-expand-lg fixed-top is-white is-dark-text">
-        <div className="navbar-brand h1 mb-0 text-large font-medium">
-          React Dashboard
-        </div>
-        <div className="navbar-nav ml-auto">
-          <div className="user-detail-section">
-            <span className="pr-2">Hi, User</span>
-            <span className="img-container">
+      <Container>
+        {/* Top static navbar */}
+        <Nav className="navbar navbar-expand-lg fixed-top is-white is-dark-text">
+          <Container className="navbar-brand h1 mb-0 text-large font-medium">
+            React Dashboard
+          </Container>
+          <Container className="navbar-nav ml-auto">
+            <Container className="user-detail-section">
+              <span className="pr-2">Hi, User</span>
+              <span className="img-container">
+              <img src={UserImg} className="rounded-circle" alt="user" />
+              </span>
+            </Container>
+          </Container>
+        </Nav>
 
-            <img src="" className="rounded-circle" alt="user" />
-             </span>
-        </div>
-        </div>
-      </Nav>
+        {/* Bottom static navbar*/}
+        <Nav className="navbar fixed-top nav-secondary is-dark is-light-text">
+          <Container className="text-medium">Summary</Container>
+          
+        </Nav>
+
+        {/* content area start */}
+        <Container className="container-fluid pr-5 pl-5 pt-5 pb-5">
+          {/* row 1 - revenue */}
+          <Container className="row">
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Total Revenue
+                  </Container>
+                </Container>
+
+                <Container className="card-value pt-4 text-x-large">
+                  
+                </Container>
+              </Container>
+            </Container>
+
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Revenue from Amazon
+                  </Container>
+                  <Container className="card-heading-brand">
+                    <i className="fab fa-amazon text-large" />
+                  </Container>
+                </Container>
+
+                <Container className="card-value pt-4 text-x-large">
+                  
+                </Container>
+              </Container>
+            </Container>
+
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Revenue from Ebay
+                  </Container>
+                  <Container className="card-heading-brand">
+                    <i className="fab fa-ebay text-x-large logo-adjust" />
+                  </Container>
+                </Container>
+
+                <Container className="card-value pt-4 text-x-large">
+                  
+                </Container>
+              </Container>
+            </Container>
+
+            <Container className="col-lg-3 col-sm-6 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Revenue from Etsy
+                  </Container>
+                  <Container className="card-heading-brand">
+                    <i className="fab fa-etsy text-medium" />
+                  </Container>
+                </Container>
+
+                <Container className="card-value pt-4 text-x-large">
+                  
+                </Container>
+              </Container>
+            </Container>
+          </Container>
+
+          {/* row 2 - conversion */}
+          <Container className="row">
+            <Container className="col-md-4 col-lg-3 is-light-text mb-4">
+              <Container className="card grid-card is-card-dark">
+                <Container className="card-heading mb-3">
+                  <Container className="is-dark-text-light letter-spacing text-small">
+                    Product Views
+                  </Container>
+                </Container>
+                <Container className="card-value pt-4 text-x-large">
+                  
+                  <span className="text-medium pl-2 is-dark-text-light">
+                    views
+                  </span>
+                </Container>
+              </Container>
+            </Container>
+
+            <Container className="col-md-8 col-lg-9 is-light-text mb-4">
+              <Container className="card is-card-dark chart-card">
+                <Container className="row full-height">
+                  <Container className="col-sm-4 full height">
+                    <Container className="chart-container full-height">
+                      
+                    </Container>
+                  </Container>
+                  <Container className="col-sm-4 full-height border-left border-right">
+                    <Container className="chart-container full-height">
+                      
+                    </Container>
+                  </Container>
+                  <Container className="col-sm-4 full-height">
+                    <Container className="chart-container full-height">
+                      
+                    </Container>
+                  </Container>
+                </Container>
+              </Container>
+            </Container>
+          </Container>
+
+          {/* row 3 - orders trend */}
+          <Container className="row" style={{ minHeight: "400px" }}>
+            <Container className="col-md-6 mb-4">
+              <Container className="card is-card-dark chart-card">
+                <Container className="chart-container large full-height">
+                  
+                </Container>
+              </Container>
+            </Container>
+
+            <Container className="col-md-6 mb-4">
+              <Container className="card is-card-dark chart-card">
+                <Container className="chart-container large full-height">
+                  
+                </Container>
+              </Container>
+            </Container>
+          </Container>
+        </Container>
+        {/* content area end */}
+
+
+      </Container>
     );
   }
 }
