1dd317f0459e239bede0f376baa11c21b54b4377
parekhjigar <hello@parekhjigar.com>
2020-06-17 22:22:00 +1000

    Updated npm packages

--- react-dashboard/src/index.js
+++ react-dashboard/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import 'react-dropdown/style.css';
+import "bootstrap/dist/css/bootstrap.css";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(
   <React.StrictMode>
